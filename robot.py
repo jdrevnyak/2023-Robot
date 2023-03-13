b52fb004fc91b530aabf45df25d83a111dbe2a99
jdrevnyak <jdrevnyak@gmail.com>
2023-03-13 17:35:56 -0400

    maybe this works with ctre

--- robot.py
+++ robot.py
@@ -1,37 +1,62 @@
 import wpilib
-import wpilib.drive
-from robotcontainer import RobotContainer
+import ctre
+import networktables
+from wpilib.drive import DifferentialDrive
+from wpilib.interfaces import GenericHID
+from networktables import NetworkTables
 
-
-class UHSRobot(wpilib.TimedRobot):
-    def robotInit(self) -> None:
-        controls = RobotContainer()
-        talons = controls.motorcontrollers()
-        controller = controls.configureButtonBindings()
+class MyRobot(wpilib.TimedRobot):
+    
+    def robotInit(self):
+        # Initialize Xbox controller on port 0
+        self.controller = wpilib.XboxController(0)
         
-        wpilib.CameraServer.launch('vision.py:main')
+        # Initialize Victor SPX motor controllers on CAN IDs 6, 7, 8, and 9
+        self.left_motor_1 = ctre.WPI_VictorSPX(6)
+        self.left_motor_2 = ctre.WPI_VictorSPX(7)
+        self.right_motor_1 = ctre.WPI_VictorSPX(8)
+        self.right_motor_2 = ctre.WPI_VictorSPX(9)
         
-        leftMotors = wpilib.MotorControllerGroup(talons.m_frontleft, talons.m_backleft)
-        rightMotors = wpilib.MotorControllerGroup(talons.m_frontright, talons.m_backright)
-        rightMotors.setInverted(True)
+        # Create speed controller groups for left and right sides
+        self.left_motors = self.left_motor_1
+        self.left_motor_2.follow(self.left_motor_1)
+        self.right_motors = self.right_motor_1
+        self.right_motor_2.follow(self.right_motor_1)
         
-        # object that handles basic drive operations
-        self.myRobot = wpilib.drive.DifferentialDrive(leftMotors, rightMotors)
-        self.myRobot.setExpiration(0.1)
-
-        # joystick #0
-        self.stick = controller
-
-    def teleopInit(self):
-        """Executed at the start of teleop mode"""
-        self.myRobot.setSafetyEnabled(True)
-
+        # Create differential drive object
+        self.drive = DifferentialDrive(self.left_motors, self.right_motors)
+        
+        # Initialize NetworkTables
+        NetworkTables.initialize(server='roborio-1537-frc.local')
+        
+        # Initialize Limelight network table
+        self.limelight_table = NetworkTables.getTable('limelight')
+        
+        # Set Limelight pipeline to use
+        self.limelight_table.putNumber('pipeline', 0)
+        
+        # Set Limelight LEDs to off
+        self.limelight_table.putNumber('ledMode', 1)
+        
     def teleopPeriodic(self):
-        """Runs the motors with tank steering"""
-        self.myRobot.arcadeDrive(
-            self.stick.getRawAxis(0), self.stick.getRawAxis(1), True
-        )
-
-
+        # Get Limelight target data
+        target_visible = self.limelight_table.getNumber('tv', 0)
+        target_x = self.limelight_table.getNumber('tx', 0)
+        target_y = self.limelight_table.getNumber('ty', 0)
+        target_area = self.limelight_table.getNumber('ta', 0)
+        
+        # Use target data for vision processing (add your own code here)
+        if target_visible:
+            # Target is visible, do something
+            pass
+        else:
+            # Target is not visible, do something else
+            pass
+        
+        # Drive robot with Xbox controller thumbstick
+        y_axis = self.controller.getRawAxis(1)
+        x_axis = self.controller.getRawAxis(4)
+        self.drive.arcadeDrive(y_axis, x_axis)
+        
 if __name__ == "__main__":
-    wpilib.run(UHSRobot)
+    wpilib.run(MyRobot)
