43621218ac0f71fb2df3d1b5642bca5f612f3424
jdrevnyak <jdrevnyak@gmail.com>
2023-03-14 17:01:26 -0400

    Maybe got camera to work?

--- vision.py
+++ vision.py
@@ -1,51 +1,37 @@
-#
-# This is a demo program showing CameraServer usage with OpenCV to do image
-# processing. The image is acquired from the USB camera, then a rectangle
-# is put on the image and sent to the dashboard. OpenCV has many methods
-# for different types of processing.
-#
-# NOTE: This code runs in its own process, so we cannot access the robot here,
-#       nor can we create/use/see wpilib objects
-#
-# To try this code out locally (if you have robotpy-cscore installed), you
-# can execute `python3 -m cscore vision.py:main`
-#
+# Import the camera server
+import cscore
+
+# Import OpenCV and NumPy
 
 import cv2
 import numpy as np
+# Initialize the camera capture
+cam = cscore.UsbCamera("camera", 0)
+cam.setResolution(640, 480)
 
-from cscore import CameraServer
+# Initialize the camera server and publish the video stream
+cs = cscore.CameraServer
+output_stream = cs.putVideo("output", 640, 480)
+output_stream.setPixelFormat(cscore.VideoMode.PixelFormat.kMJPEG)
 
+# Initialize the human detection algorithm (here we use OpenCV's HOGDescriptor)
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-def main():
-    cs = CameraServer.getInstance()
-    cs.enableLogging()
+# Start the video capture loop
+while True:
+    # Capture a frame from the camera
+    frame = np.zeros(shape=(480, 640, 3), dtype=np.uint8)
+    cam.grabFrameNoTimeout(frame)
+    if cam.getError() != "":
+        print(cam.getError())
 
-    camera = cs.startAutomaticCapture()
+    # Apply the human detection algorithm to the frame
+    found, _ = hog.detectMultiScale(frame)
 
-    camera.setResolution(320, 240)
+    # Draw bounding boxes around the detected humans
+    for (x, y, w, h) in found:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-    # Get a CvSink. This will capture images from the camera
-    cvSink = cs.getVideo()
-
-    # (optional) Setup a CvSource. This will send images back to the Dashboard
-    outputStream = cs.putVideo("Rectangle", 320, 240)
-
-    # Allocating new images is very expensive, always try to preallocate
-    img = np.zeros(shape=(240, 320, 3), dtype=np.uint8)
-
-    while True:
-        # Tell the CvSink to grab a frame from the camera and put it
-        # in the source image.  If there is an error notify the output.
-        time, img = cvSink.grabFrame(img)
-        if time == 0:
-            # Send the output the error.
-            outputStream.notifyError(cvSink.getError())
-            # skip the rest of the current iteration
-            continue
-
-        # Put a rectangle on the image
-        cv2.rectangle(img, (100, 100), (300, 300), (255, 255, 255), 5)
-
-        # Give the output stream a new image to display
-        outputStream.putFrame(img)
+    # Publish the processed frame to the camera server
+    output_stream.putFrame(frame)
